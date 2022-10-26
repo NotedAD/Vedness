638b8ee387668b947a39711f241651426ac33d0e
Niksonskywalker <nikita.strakh250505@mail.ru>
2022-10-26 15:10:39 +0300

    0.0.1
    НАЧАЛО НАЧАЛ

---
+++ Vedness/Assets/Asset/Environment/Map/all_grass24x24/24x24dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="24x24dungeon" tilewidth="24" tileheight="24" tilecount="800" columns="8">
+ <image source="24x24_dungeon.png" trans="ff00ff" width="192" height="2400"/>
+</tileset>
